0df9b77b3624a4924a2778bb39672c8647439074
shitianen <ShiTianEn52@126.com>
2020-08-14 18:18:31 +0800

    溢出文件

---
+++ MyProject/src/com/itcast/cn/Test.java
@@ -0,0 +1,19 @@
+package com.itcast.cn;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("你好");
+
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("" +
+                " System.out.println(\"你好\");" +
+                "你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        String I = "2323";
+        System.out.println("你好"); System.out.println("你好");
+
+    }
+}
