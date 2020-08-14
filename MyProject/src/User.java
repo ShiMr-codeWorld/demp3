5ee817a987d6f6dcaff3f8bc316c3395e237d5db
shitianen <ShiTianEn52@126.com>
2020-08-14 18:10:39 +0800

    忽略提交

---
+++ MyProject/src/User.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class User {
+    private int id;
+    private int username;
+    public static  void fun(){
+        /*System.error.print();
+        System.error.print();
+        System.error.print();
+        System.error.print();*/
+//        System.error.print();
+
+
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, Integer> leihao = new HashMap<>();
+        leihao.put("nihao", 123);        
+
+
+    }
+}
